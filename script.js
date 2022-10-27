fec3c580b27c6e3a09bb784527854d8b9af130e2
JustVika <just_vika_248@bk.ru>
2022-10-27 22:54:49 +0300

    change1

--- script.js
+++ script.js
@@ -20,67 +20,63 @@ class SearchRepositories {
         this.repositoriesData = []
     }
     async search() {
-        const value = this.nameRepository.value
-        if (value.trim()) {
-            return await fetch(`https://api.github.com/search/repositories?q=${value}&per_page=5`)
-                .then((rep) => {
-
-                    if (rep.ok) {
-                        this.clearfoundRepositories()
-                        rep.json().then(rep => {
-                            if (!rep.items.length) this.foundRepositories.style.height = this.repositoriesData.length * 40 + 'px'
-                            rep.items.forEach(repositories => {
-                                this.repositoriesData.push({
-                                    id: repositories.id,
-                                    name: repositories.name,
-                                    owner: repositories.owner.login,
-                                    stars: repositories['stargazers_count'],
-                                })
-                                const item = this.createElement('li', 'listFoundRepositories__item')
-                                item.innerHTML = repositories.name;
-                                item.dataset.id = repositories.id
-                                this.foundRepositories.append(item)
-                                this.foundRepositories.style.height = this.repositoriesData.length * 40 + 'px'
-                            })
-                        })
-                    }
-                })
-
-        }
-        else {
+        const value = this.nameRepository.value.trim()
+        if (!value) {
             this.clearfoundRepositories()
-            this.foundRepositories.style.height = this.repositoriesData.length * 40 + 'px'
+            this.foundRepositories.style.height = '0px'
+            return
         }
+        const requestGit = await fetch(`https://api.github.com/search/repositories?q=${value}&per_page=5`)
+        if (!requestGit.ok) return
+        this.clearfoundRepositories()
+        const rep = await requestGit.json()
+        console.log(rep.items)
+        if (!rep.items.length) this.foundRepositories.style.height = '0px'
+        rep.items.forEach(repositories => {
+            this.repositoriesData.push({
+                id: repositories.id,
+                name: repositories.name,
+                owner: repositories.owner.login,
+                stars: repositories['stargazers_count'],
+            })
+            const item = this.createElement('li', 'listFoundRepositories__item')
+            const btnItem =  this.createElement('button', 'listFoundRepositories__btn')
+            btnItem.innerHTML = repositories.name;
+            btnItem.dataset.id = repositories.id
+            item.append(btnItem)
+            this.foundRepositories.append(item)
+            this.foundRepositories.style.height = this.repositoriesData.length * 40 + 'px'
+        })
     }
 
     addRepositories(elem) {
-        if (elem.target.className == "listFoundRepositories__item") {
-            const [addElemnt] = this.repositoriesData.filter((element) => element.id == elem.target.dataset.id)
-            if (this.repositoriesDataId.indexOf(addElemnt.id) != -1) {
-                this.clearfoundRepositories()
-                this.foundRepositories.style.height = this.repositoriesData.length * 40 + 'px'
-                this.nameRepository.value = ''
-                return true
-            }
-            this.repositoriesDataId.push(addElemnt.id)
-            const item = this.createElement('li', 'listSavedRepositories__item');
-            item.innerHTML = `<ul class="listInfoRepositories">
-                                <li class="listInfoRepositories__item">Name: ${addElemnt.name}</li>
-                                <li class="listInfoRepositories__item">Owner: ${addElemnt.owner}</li>
-                                <li class="listInfoRepositories__item">Stars: ${addElemnt.stars}</li>
-                             </ul>`
-            this.SavedRepositories.append(item)
-            const btn = this.createElement('button', 'repositories__button');
-            btn.addEventListener('click', () => {
-                btn.parentElement.remove()
-                this.repositoriesDataId.pop(addElemnt.id)
-            })
-            item.append(btn)
+        if (elem.target.className != "listFoundRepositories__btn") return false
+        const [addElemnt] = this.repositoriesData.filter((element) => element.id == elem.target.dataset.id)
+        if (this.repositoriesDataId.indexOf(addElemnt.id) != -1) {
             this.clearfoundRepositories()
-            this.foundRepositories.style.height = this.repositoriesData.length * 40 + 'px'
+            this.foundRepositories.style.height = '0px'
             this.nameRepository.value = ''
             return true
         }
+        this.repositoriesDataId.push(addElemnt.id)
+        const item = this.createElement('li', 'listSavedRepositories__item');
+        item.innerHTML = `<div class="repositories__infoRepositories">
+                                <div class="infoRepositories">Name: ${addElemnt.name}</div>
+                                <div class="infoRepositories">Owner: ${addElemnt.owner}</div>
+                                <div class="infoRepositories">Stars: ${addElemnt.stars}</div>
+                             </div>`
+        this.SavedRepositories.append(item)
+        const btn = this.createElement('button', 'repositories__button');
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove()
+            this.repositoriesDataId.pop(addElemnt.id)
+        })
+        item.append(btn)
+        this.clearfoundRepositories()
+        this.foundRepositories.style.height = this.repositoriesData.length * 40 + 'px'
+        this.nameRepository.value = ''
+        return true
+
     }
 
     debounce(fn, debounceTime) {
